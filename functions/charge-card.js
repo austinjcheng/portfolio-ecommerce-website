dc662439ecda241dde1e1db3f4746943d2a3c7f4
Austin Cheng <austinjcheng@gmail.com>
2023-03-15 20:55:23 -0700

    Update charge-card to match current Stripe api

--- functions/charge-card.js
+++ functions/charge-card.js
@@ -1,4 +1,5 @@
-const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`);
+const Stripe = require("stripe");
+const stripe = Stripe(`${process.env.STRIPE_SECRET_KEY}`);
 const products = require("./products.json");
 
 exports.handler = async (event, context) => {
